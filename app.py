bb37a02584c0790e788acb37bde048e5d2646e57
Sanchith Hegde <sanchith.hegde01@gmail.com>
2020-12-16 00:44:16 +0530

    app: Remove all logging config

--- app.py
+++ app.py
@@ -1,33 +1,8 @@
-from logging.config import dictConfig
-
 import numpy as np
 from flask import Flask, render_template, request
 from PIL import Image
 
 from predict import find_student
-
-dictConfig(
-    {
-        "version": 1,
-        "formatters": {
-            "default": {
-                "format": "%(asctime)s - %(levelname)s - %(module)s: %(message)s",
-            }
-        },
-        "handlers": {
-            "wsgi": {
-                "class": "logging.StreamHandler",
-                "stream": "ext://flask.logging.wsgi_errors_stream",
-                "formatter": "default",
-            }
-        },
-        "root": {
-            "level": "INFO",
-            "handlers": ["wsgi"],
-        },
-    }
-)
-
 
 app = Flask(__name__)
 
